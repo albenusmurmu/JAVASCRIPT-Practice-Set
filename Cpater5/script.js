64df8161276276d51d3903559ded825f4543c268
Albenus Pter Murmu <146658653+albenusmurmu@users.noreply.github.com>
2024-05-18 17:06:57 +0530

    Cpater5

---
+++ Cpater5/script.js
@@ -0,0 +1,35 @@
+
+// ! chapter 5
+
+// * Qs1:- Create a program that generates a random number representing a dice roll.
+// * [The number shuld be between 1 and 6].
+    
+//  let dice = Math.floor(Math.random() * 6) + 1;
+//  console.log(dice);
+
+// * Qs2:- Create an object representing a car that stores the following properties for the 
+// * Pirnt the car's name.
+
+// let car = {
+//    name: "Audi",
+//    model:"A4",
+//    color: "Black",
+//    year: 2020,
+// }
+//  console.log(car);
+
+// * Qs3:-create an Object Person with their name, age and city.
+// * Edit their city's original value to change it to "New York".
+// * Add a new property country and set it to the United States.
+
+
+// const person ={
+//     name: "peter",
+//     age:20,
+//     city: "jaipur",
+// }
+// // console.log(person);
+// person.city = "New York";
+// // console.log(person);
+// person.newCountry = "United State";
+// console.log(person)
